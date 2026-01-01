5c50d5f9813c0711056c3d0a66e64f30830ffab8
Kiran Kumar <kirankumar.bhalekar@gmail.com>
2026-01-01 21:20:35 +0100

    Fix issues for package.json file

--- src/app.js
+++ src/app.js
@@ -97,12 +97,12 @@ app.put("/user/:id", async (req, res) => {
 });
 
 connectDB().then(() => {
-    console.log("Connected to MongoDB");
     app.listen(3000, () => {
         console.log("Server is running on port 3000");
     });
 }).catch((err) => {
-    console.log("Error connecting to MongoDB");
-    console.error(err);
+    console.error("Failed to start server:", err);
+    process.exit(1);
 });
 
+module.exports = app;
